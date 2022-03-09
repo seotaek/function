a6bc8f873ff197a9dfe7953f186ee562f74a212a
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-09 20:20:42 +0900

    코딩도장67.6

---
+++ function67.6/function67.6/function67.6.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int fib(int n) {
+    if (n <= 1)
+        return n;
+   
+
+    return (fib(n-1) + fib(n-2));
+}
+
+int main()
+{
+    int num1;
+
+    scanf("%d", &num1);
+
+    printf("%d\n", fib(num1));
+
+    return 0;
+}
